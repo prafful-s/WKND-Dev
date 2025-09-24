4d02a2a120d2c04baf7f520c8ddd4d73c84e0f3b
pankaj2186 <pansingh@adobe.com>
2025-09-24 13:41:12 +0530

    Changed code to load AT only for preview
    Added a check to call loadAT() only if page is not under authoring. This is done so that targeting doesn't happen in authoring.

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -102,5 +102,7 @@ function buildTwitterLinks() {
 if (!window.location.hostname.includes('localhost')) {
   
   embedCustomLibraries();
-  loadAT();
+  if (!(window.location.href.indexOf('/canvas/') > -1)) {
+    loadAT();
+  }
 }
